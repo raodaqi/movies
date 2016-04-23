f200f33e0ca43a8a1296cf1a8a9c86db1eaa7a5e
raodaqi <raodaqi@qq.com>
2016-04-24 03:21:57 +0800

    bug提醒

--- cloud/main.js
+++ cloud/main.js
@@ -6,30 +6,30 @@ AV.Cloud.define("getData", function(request, response) {
   // appJs.cronGetData();
   appJs.getCUITImportentData();
   // appJs.getJWCImportentData();
-  response.success("Log in timer.");
+  console.log("Log in timer.");
 });
 
 AV.Cloud.define("moviesDelete", function(request, response) {
   appJs.moviesDelete();
-  response.success("清空movies里的所有数据");
+  console.log("清空movies里的所有数据");
 });
 
 AV.Cloud.define("getMoviesData", function(request, response) {
   appJs.getMoviesData();
-  response.success("获取电影列表");
+  console.log("获取电影列表");
 });
 
 AV.Cloud.define("getBJMovies", function(request, response) {
   appJs.getBJMovies();
-  response.success("获取比价网电影列表");
+  console.log("获取比价网电影列表");
 });
 
 AV.Cloud.define("getNMNewMovie", function(request, response) {
   appJs.getNMNewMovie();
-  response.success("获取百度糯米正在上映电影");
+  console.log("获取百度糯米正在上映电影");
 });
 
 AV.Cloud.define("sendLowPriceEmail", function(request, response) {
   appJs.sendLowPriceEmail();
-  response.success("发送低价邮件");
+  console.log("发送低价邮件");
 });
