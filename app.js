8836d653064caa34168c4e5f50ef5c9f903701ab
raodaqi <raodaqi@qq.com>
2016-06-05 19:36:52 +0800

    电影bid

--- app.js
+++ app.js
@@ -1587,6 +1587,58 @@ app.get('/getprice', function(req, res) {
 
 });
 
+app.get('/getpricebyid', function(req, res) {
+  var bid =  req.query.bid;
+  // console.log(id);
+  // getBJId(movie,{
+  //   success:function(result){
+  //     console.log(result);
+  //     var bid = result.attributes.bid;
+      if(!bid){
+        var result = {
+          code : 600,
+          message: "还未出来价格"
+        }
+        res.send(result);
+      }else{
+        var date = new Date().getTime();
+        date = formatDate("y-m-d",date);
+        AV.Cloud.httpRequest({
+       //    url: 'http://www.xuerendianying.com/bijia_api/fs/filmsessionlist/?movie=326929&cinema=4838&date=2016-03-25',
+         url: 'http://www.xuerendianying.com/bijia_api/fs/filmsessionlist/?movie='+bid+'&cinema=4838&date='+date,
+         headers: {
+         },
+         success: function(httpResponse) {
+           console.log("success");
+          //  console.log(httpResponse.text);
+           var BJData = eval("BJData="+httpResponse.text);
+           var priceData = {};
+           console.log(BJData);
+           var minData = BJData.filmsession_list[0];
+           priceData.name = BJData.movie.title;
+            var result = {
+              code : 200,
+              message: "",
+              list:BJData.filmsession_list
+            }
+           res.send(result);
+         },
+         error:function(error){
+            var result = {
+              code : 700,
+              message: ""
+            }
+            res.send(result);
+         }
+       });
+      }
+    // },
+  //   error:function(error){
+  //   }
+  // });
+
+});
+
 
 
 
